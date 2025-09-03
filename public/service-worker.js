e09d1966eca44220f4ca651513a1df419d38facc
Rohan D V <153365107+Rohan300520@users.noreply.github.com>
2025-09-03 23:19:44 +0530

    feat: Add PWA support with service worker
    Implements basic Progressive Web App (PWA) capabilities by adding a service worker and updating the manifest.json. This enables offline caching of essential assets and allows the application to be added to the home screen.

---
+++ public/service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'place-value-playbox-v2';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/icon.svg'
+];
+
+// Install the service worker and cache the essential app assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch assets from the cache first, falling back to the network
+self.addEventListener('fetch', event => {
+  // We only want to cache GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response from the cache
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
